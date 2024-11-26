5d12649df692b3454b75e8605a003079cc3a8faa
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-26 02:32:03 +0200

    fetching API

--- podcast-cloud/src/App.jsx
+++ podcast-cloud/src/App.jsx
@@ -1,5 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function App() {}
+function App() {
+    const[data, setData] = useState([]) //data=store fetched data, setData=update 
+    const [ loading , setLoading] = useState(true) //loading=data still being fetched? , setLoading=update
+    const[error,setError] = useState(null) //error=stores errors while fetching , setError=update
+
+    useEffect(() => { 
+        fetch("https://podcast-api.netlify.app/") //fetch data from endpoint
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error ('Something went wrong')
+            }
+            return res.json() //res => JSON
+        })
+    } )
+    .then((data) => {
+        setData(data) //update with fetched data
+        setLoading(false)  //fetching is done
+    })
+    .catch((error) => {
+        setError(error)  //store error
+        setLoading(false)
+         })
+        }, []) //to only run once
+
+        if (loading) return <p>Loading...</p>
+        if (error) return <p>{error.message}</p>
+
+
+
 
 export default App;
