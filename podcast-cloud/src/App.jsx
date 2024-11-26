b8702f2ee4a589a36aa728cea4f488782fe9b538
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-26 03:17:38 +0200

    fixing errors

--- podcast-cloud/src/App.jsx
+++ podcast-cloud/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 
 function App() {
+    //state vars
     const[data, setData] = useState([]) //data=store fetched data, setData=update 
     const [ loading , setLoading] = useState(true) //loading=data still being fetched? , setLoading=update
     const[error,setError] = useState(null) //error=stores errors while fetching , setError=update
 
-    useEffect(() => { 
+    useEffect(() => { //endpoint data
+
         fetch("https://podcast-api.netlify.app/") //fetch data from endpoint
         .then((res) => {
             if (!res.ok) {
@@ -13,11 +15,11 @@ function App() {
             }
             return res.json() //res => JSON
         })
-    } )
-    .then((data) => {
-        setData(data) //update with fetched data
-        setLoading(false)  //fetching is done
+        .then((data) => {
+            setData(data) //update with fetched data
+            setLoading(false)  //fetching is done
     })
+   
     .catch((error) => {
         setError(error)  //store error
         setLoading(false)
@@ -27,7 +29,10 @@ function App() {
         if (loading) return <p>Loading...</p>
         if (error) return <p>{error.message}</p>
 
+//rendering podcast data
+return(
 
+)
 
-
+}
 export default App;
