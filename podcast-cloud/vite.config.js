2c6e03fd367ec08aa58878248926d0267fb88242
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-27 04:18:04 +0200

    fixing build error of netlify

--- podcast-cloud/vite.config.js
+++ podcast-cloud/vite.config.js
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  resolve: {
+    alias: {
+      components: "/src/components",
+    },
+  },
   build: {
     outDir: "podcast-cloud/dist",
     emptyOutDir: true,
