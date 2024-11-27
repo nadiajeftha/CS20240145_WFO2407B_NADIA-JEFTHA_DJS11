b19f526f1cd31aa39262f14f6e59a2652c3d699e
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-27 03:56:40 +0200

    babel

--- podcast-cloud/src/components/App.jsx
+++ podcast-cloud/src/components/App.jsx
@@ -1,38 +1,35 @@
 import { useState, useEffect } from "react";
 
 function App() {
-    //state vars
-    const[data, setData] = useState([]) //data=store fetched data, setData=update 
-    const [ loading , setLoading] = useState(true) //loading=data still being fetched? , setLoading=update
-    const[error,setError] = useState(null) //error=stores errors while fetching , setError=update
+  //state vars
+  const [data, setData] = useState([]); //data=store fetched data, setData=update
+  const [loading, setLoading] = useState(true); //loading=data still being fetched? , setLoading=update
+  const [error, setError] = useState(null); //error=stores errors while fetching , setError=update
 
-    useEffect(() => { //endpoint data
+  useEffect(() => {
+    //endpoint data
 
-        fetch("https://podcast-api.netlify.app/") //fetch data from endpoint
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error ('Something went wrong')
-            }
-            return res.json() //res => JSON
-        })
-        .then((data) => {
-            setData(data) //update with fetched data
-            setLoading(false)  //fetching is done
-    })
-   
-    .catch((error) => {
-        setError(error)  //store error
-        setLoading(false)
-         })
-        }, []) //to only run once
+    fetch("https://podcast-api.netlify.app/") //fetch data from endpoint
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Something went wrong");
+        }
+        return res.json(); //res => JSON
+      })
+      .then((data) => {
+        setData(data); //update with fetched data
+        setLoading(false); //fetching is done
+      })
 
-        if (loading) return <p>Loading...</p>
-        if (error) return <p>{error.message}</p>
+      .catch((error) => {
+        setError(error); //store error
+        setLoading(false);
+      });
+  }, []); //to only run once
 
-//rendering podcast data
-return(
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
 
-)
-
-}ASS
+  //rendering podcast data
+}
 export default App;
