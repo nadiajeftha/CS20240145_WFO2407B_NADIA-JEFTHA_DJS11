e971643ed2cd2b9e3015bf5ca65560360731dc1c
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-28 19:08:35 +0200

    moved genre filtering buttons to its own component

--- podcast-cloud/src/components/App.jsx
+++ podcast-cloud/src/components/App.jsx
@@ -53,22 +53,7 @@ function App() {
       {!loading && !error && (
         <div>
           <h1>Podcast Cloud</h1>
-          <div>
-            {" "}
-            {/*buttons to filter genre*/}
-            <button onClick={() => filterByGenre(null)}>All Genres</button>
-            <button onClick={() => filterByGenre(1)}>Personal Growth</button>
-            <button onClick={() => filterByGenre(2)}>
-              Investigative Journalism
-            </button>
-            <button onClick={() => filterByGenre(3)}>History</button>
-            <button onClick={() => filterByGenre(4)}>Comedy</button>
-            <button onClick={() => filterByGenre(5)}>Entertainment</button>
-            <button onClick={() => filterByGenre(6)}>Business</button>
-            <button onClick={() => filterByGenre(7)}>Fiction</button>
-            <button onClick={() => filterByGenre(8)}>News</button>
-            <button onClick={() => filterByGenre(9)}>Kids & Family</button>
-          </div>
+          <div> {/*buttons to filter genre*/}</div>
 
           {/*list of shows*/}
           <div>
---
+++ podcast-cloud/src/components/GenreFilterBtn.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const GenreFilter = ({ filterByGenre }) => {
+  return (
+    <div>
+      <button onClick={() => filterByGenre(null)}>All Genres</button>
+      <button onClick={() => filterByGenre(1)}>Personal Growth</button>
+      <button onClick={() => filterByGenre(2)}>Investigative Journalism</button>
+      <button onClick={() => filterByGenre(3)}>History</button>
+      <button onClick={() => filterByGenre(4)}>Comedy</button>
+      <button onClick={() => filterByGenre(5)}>Entertainment</button>
+      <button onClick={() => filterByGenre(6)}>Business</button>
+      <button onClick={() => filterByGenre(7)}>Fiction</button>
+      <button onClick={() => filterByGenre(8)}>News</button>
+      <button onClick={() => filterByGenre(9)}>Kids & Family</button>
+    </div>
+  );
+};
+export default GenreFilter;
