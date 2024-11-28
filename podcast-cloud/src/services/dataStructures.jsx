2484e755e1e5292ef6c623934c20639e2c40f980
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-28 23:17:58 +0200

    data structures

---
+++ podcast-cloud/src/services/dataStructures.jsx
@@ -0,0 +1,42 @@
+import { description } from "commander";
+import { id } from "prelude-ls";
+import { setFlagsFromString } from "v8";
+
+export const Preview = {
+    id: 0,
+    title: "",
+    description:"",
+    seasons: 0,
+    image:"",
+    genreIds: [],
+    updated: ""
+}
+
+export const Show = {
+    id: 0,
+    title:"",
+    description: "",
+    seasons:[]
+}
+
+export const Season = {
+    id: 0,
+    title: "", 
+    image: "", 
+    episodes: [],
+}
+
+export const Episodes = {
+    id:0,
+    file:"",
+    title:""
+}
+
+export const Genre = {
+    id:0,
+    title:"",
+    description: "",
+    showIds: []
+}
+
+export const 
