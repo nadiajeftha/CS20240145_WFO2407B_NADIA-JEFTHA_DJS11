63957f7b94fecbd95e9bdaa39b7575cac332b341
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-28 15:41:03 +0200

    trying to fix blank page

--- podcast-cloud/src/index.jsx
+++ podcast-cloud/src/index.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "components/App";
+import { BrowserRouter, Routes, Route } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <App />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
