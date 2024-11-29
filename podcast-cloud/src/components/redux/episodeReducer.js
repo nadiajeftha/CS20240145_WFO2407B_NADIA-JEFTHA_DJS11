484694c84a02ec0eed370696e49a6cff92781ed3
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-29 12:23:35 +0200

    reducers structure

---
+++ podcast-cloud/src/components/redux/episodeReducer.js
@@ -0,0 +1,23 @@
+const initialState= {
+    currentEpisode:null,
+    isPlaying:false,
+    episodeQueue:[],
+}
+const episodeReducer=() => {
+    switch() {
+        case
+        return
+    }
+}
+case
+return {
+
+}
+case
+return {
+
+}
+default
+    return
+    
+export default episodeReducer
---
+++ podcast-cloud/src/components/redux/favouriteReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+    favourite: [],
+}
+
+const favouriteReducer = () =>
+    switch(){
+case 
+    return {
+
+    }
+} 
+case
+    return {
+    
+    }
+
+    default:
+        return state
+
+export default favouriteReducer
