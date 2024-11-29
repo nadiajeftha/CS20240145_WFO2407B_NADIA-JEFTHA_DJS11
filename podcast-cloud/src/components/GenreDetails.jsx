3d10cf69dfdc7cd7448b5332bbd7e376ac4eacaa
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-29 07:07:22 +0200

    GenreDetails structure

---
+++ podcast-cloud/src/components/GenreDetails.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { podcastService } from "../services/podcastService";
+
+useEffect(() => {
+    const fetchGenreDetails = async () => {
+        try {
+
+        } catch (error)
+    }
+}
+)
+fetchGenreDetails()
+
