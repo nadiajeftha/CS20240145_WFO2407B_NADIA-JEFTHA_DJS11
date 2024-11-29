04a81df835ff798e6a908237cf973920e4ec167a
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-29 14:00:26 +0200

    structure of player component

---
+++ podcast-cloud/src/components/EpisodePlayer.jsx
@@ -0,0 +1,37 @@
+import React,{useEffect} from "react"
+import {useSelector,useDispatch} from "react-redux"
+import {setCurrentEpisode,togglePlayPause} from "./redux/episodeActions"
+
+const EpisodePlayer =() => {
+
+}
+const EpisodePlayer=() =>{
+
+}
+
+const handlePlayPause=() => {
+
+}
+
+const handleNext=() => {
+
+}
+
+const handle Previous=() => {
+
+}
+
+useEffect(() => {
+    if() {
+        if() {
+
+        }else {
+
+        }}
+})
+
+return (
+
+)
+
+export default EpisodePlayer
