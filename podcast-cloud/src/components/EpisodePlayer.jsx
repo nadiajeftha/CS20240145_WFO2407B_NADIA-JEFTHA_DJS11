0e11bc4c96a95d26f34c551c621185f72470485e
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-29 14:18:25 +0200

    episode player

--- podcast-cloud/src/components/EpisodePlayer.jsx
+++ podcast-cloud/src/components/EpisodePlayer.jsx
@@ -3,35 +3,46 @@ import {useSelector,useDispatch} from "react-redux"
 import {setCurrentEpisode,togglePlayPause} from "./redux/episodeActions"
 
 const EpisodePlayer =() => {
+    const dispatch= useDispatch()
+    const {setCurrentEpisode,isPlaying,episodeQueue} = useSelector(state => state.episode)
 
-}
-const EpisodePlayer=() =>{
-
-}
-
-const handlePlayPause=() => {
-
+    const handlePlayPause=() => {
+        dispatch(togglePlayPause())
+    }
 }
 
 const handleNext=() => {
-
+    const currentIndex=episodeQueue.findIndex(ep.id === setCurrentEpisode.id)
+    const nextEpisode=episodeQueue[(currentIndex + 1)  % episodeQueue.length]
+    dispatch(setCurrentEpisode(nextEpisode))
 }
 
-const handle Previous=() => {
-
+const handlePrevious=() => {
+    const currentIndex=episodeQueue.findIndex(ep.id === setCurrentEpisode.id)
+    const previousEpisode=episodeQueue[(currentIndex - 1 + episodeQueue.length)  % episodeQueue.length]
+    dispatch(setCurrentEpisode(previousEpisode))
 }
 
 useEffect(() => {
-    if() {
-        if() {
+    if(currentEpisode) {
+        if(isPlaying) {
 
         }else {
 
         }}
-})
+}, [currentEpisode,isPlaying])
 
 return (
-
-)
+<div>
+    
+    <h3>{currentEpisode.title}</h3>
+    <button onClick={handlePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
+    <button onClick={handlePrevious}>Previous</button>
+    <button onClick={handleNext}>Next</button>
+</div>
+) : (
+    <p>Select an episode to play</p>
+)}
+</div>
 
 export default EpisodePlayer
