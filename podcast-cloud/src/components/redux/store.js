f53d079afd80bedbd8b1232482150dc8cdf6012c
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-29 13:23:52 +0200

    created store

--- podcast-cloud/src/components/redux/store.js
+++ podcast-cloud/src/components/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import favouritesReducer from "./favouritesReducer";
+import episodeReducer from "./episodeReducer";
 
-const store = createStore(rootReducer);
+const store = configureStore({
+  reducer: {
+    episodes: episodeReducer,
+    favourites: favouritesReducer,
+  },
+});
 
 export default store;
