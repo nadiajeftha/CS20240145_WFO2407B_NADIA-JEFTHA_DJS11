a07a0b2c844ab1c88a2b48be2c5a671e1f374914
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-29 13:39:45 +0200

    fixed index.jsx

--- podcast-cloud/src/index.jsx
+++ podcast-cloud/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {Provider} from "react-redux"
-import { BrowserRouter, Routes, Route } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
 import store from "./components/redux/store"
